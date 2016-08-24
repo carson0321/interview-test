605e6449daf3ac32e9fdba219259b906d7368d91
carson <r03944040@csie.ntu.edu.tw>
2016-08-25 00:42:09 +0800

    update

---
+++ Assets/InputController.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputController : MonoBehaviour {
+
+    private bool isMobile = true;
+    private PandaController _player;
+
+    // Use this for initialization
+    void Start()
+    {
+        if (Application.isEditor)
+            isMobile = false;
+        _player = GameObject.Find("Panda").GetComponent<PandaController>();
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (isMobile)
+        {
+            int tmp = Input.touchCount;
+            tmp--;
+
+            if (Input.GetTouch(tmp).phase == TouchPhase.Began) handleInteraction(true);
+            if (Input.GetTouch(tmp).phase == TouchPhase.Ended) handleInteraction(false);
+        }
+        else
+        {
+            if (Input.GetMouseButtonDown(0)) handleInteraction(true);
+            if (Input.GetMouseButtonUp(0)) handleInteraction(false);
+        }
+    }
+
+    void handleInteraction(bool starting)
+    {
+        if (starting)  _player.jump();
+        else _player.jumpPress = false;
+    }
+}
---
+++ Assets/PandaController.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PandaController : MonoBehaviour {
+    //panda's run speed and animation
+    public Sprite[] playerSprite = new Sprite[4];
+    public SpriteRenderer mainRender;
+    public float time = 0;
+    public int change;
+    public float speed = 0.05f;
+
+
+    private bool inAir = false;
+    public bool jumpPress = false;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        moveAnimation();
+        gameObject.transform.position += new Vector3(speed, 0, 0);
+        if (!inAir && Mathf.Abs(this.GetComponent<Rigidbody2D>().velocity.y) > 0.05f)  inAir = true;
+        else if (inAir && this.GetComponent<Rigidbody2D>().velocity.y == 0.00f)
+        {
+            inAir = false;
+            //transform.Translate(Vector3.forward * speed * Time.deltaTime);
+            if (jumpPress) jump();
+        }
+    }
+
+    private void moveAnimation()
+    {
+        if (time > 4) time = 0;
+        time += 10 * Time.deltaTime;
+        change = (int)time;
+        mainRender.sprite = playerSprite[change];
+    }
+
+    public void jump()
+    {
+        jumpPress = true;
+        if (inAir) return;
+        this.GetComponent<Rigidbody2D>().AddForce(Vector2.up * 3000);
+    }
+}
