75b66ec207e5f4c5df0a38e69fd2d4a51e4b0ebe
carson <r03944040@csie.ntu.edu.tw>
2016-08-28 02:50:28 +0800

    good solution version

--- Assets/LevelCreator.cs
+++ Assets/LevelCreator.cs
@@ -8,13 +8,26 @@ public class LevelCreator : MonoBehaviour {
     public GameObject tileRoadPos;
     public GameObject tileKillBottomPos;
     public GameObject tileKillTopPos;
-
+    public GameObject tileTree1Pos;
+    public GameObject tileTree2Pos;
+    public GameObject tileCloud1Pos;
+    public GameObject tileCloud2Pos;
+    public GameObject tileTreeShadowPos;
+    public GameObject tileGrass1Pos;
+    public GameObject tileGrass2Pos;
 
     private GameObject gameLayer;
     private GameObject tmpTile;
     private float startUpRoadPosY;
     private float startUpKillBottomPosY;
     private float startUpKillTopPosY;
+    private float startUpTree1PosY;
+    private float startUpTree2PosY;
+    private float startUpCloud1PosY;
+    private float startUpCloud2PosY;
+    private float startUpTreeShadowPosY;
+    private float startUpGrass1PosY;
+    private float startUpGrass2PosY;
 
     public float gameSpeed = 100.0f;
     private float outofbounceX;
@@ -27,23 +40,20 @@ public class LevelCreator : MonoBehaviour {
         gameLayer = GameObject.Find("gameLayer");
         collectedTiles = GameObject.Find("tmp");
 
-        for (int i = 0; i < 10; i++)
-        {
-            GameObject tmpRoad = Instantiate(Resources.Load("Road", typeof(GameObject))) as GameObject;
-            tmpRoad.transform.parent = collectedTiles.transform.FindChild("tmpRoad").transform;
-            tmpRoad.transform.position = Vector2.zero;
-            
-        }
+        for (int i = 0; i < 10; i++) setGameObject("Road");
+         
 
         for (int i = 0; i < 10; i++)
         {
-            GameObject tmpKillBottom = Instantiate(Resources.Load("killBottom", typeof(GameObject))) as GameObject;
-            tmpKillBottom.transform.parent = collectedTiles.transform.FindChild("tmpKillBottom").transform;
-            tmpKillBottom.transform.position = Vector2.zero;
-
-            GameObject tmpKillTop = Instantiate(Resources.Load("killTop", typeof(GameObject))) as GameObject;
-            tmpKillTop.transform.parent = collectedTiles.transform.FindChild("tmpKillTop").transform;
-            tmpKillTop.transform.position = Vector2.zero;
+            setGameObject("KillBottom");
+            setGameObject("KillTop");
+            setGameObject("Tree1");
+            setGameObject("Tree2");
+            setGameObject("Cloud1");
+            setGameObject("Cloud2");
+            setGameObject("TreeShadow");
+            setGameObject("Grass1");
+            setGameObject("Grass2");
         }
 
         collectedTiles.transform.position = new Vector2(-60.0f, -20.0f);
@@ -58,10 +68,40 @@ public class LevelCreator : MonoBehaviour {
         tileKillTopPos = GameObject.Find("startTop");
         startUpKillTopPosY = tileKillTopPos.transform.position.y;
 
+        tileTree1Pos = GameObject.Find("startTree1");
+        startUpTree1PosY = GameObject.Find("startTree1").transform.position.y;
+
+        tileTree2Pos = GameObject.Find("startTree2");
+        startUpTree2PosY = GameObject.Find("startTree2").transform.position.y;
+
+        tileCloud1Pos = GameObject.Find("startCloud1");
+        startUpCloud1PosY = GameObject.Find("startCloud1").transform.position.y;
+
+        tileCloud2Pos = GameObject.Find("startCloud2");
+        startUpCloud2PosY = GameObject.Find("startCloud2").transform.position.y;
+
+        tileTreeShadowPos = GameObject.Find("startTreeShadow");
+        startUpTreeShadowPosY = GameObject.Find("startTreeShadow").transform.position.y;
+
+        tileGrass1Pos = GameObject.Find("startGrass1");
+        startUpGrass1PosY = GameObject.Find("startGrass1").transform.position.y;
+
+        tileGrass2Pos = GameObject.Find("startGrass2");
+        startUpGrass2PosY = GameObject.Find("startGrass2").transform.position.y;
+
+
+
         fillScene();
         randomizeEnemy();
     }
 	
+    private void setGameObject(string type)
+    {
+        GameObject tmpObject = Instantiate(Resources.Load(type, typeof(GameObject))) as GameObject;
+        tmpObject.transform.parent = collectedTiles.transform.FindChild("tmp"+ type).transform;
+        tmpObject.transform.position = Vector2.zero;
+    }
+
 	// Update is called once per frame
 	void Update () {
 	
@@ -81,13 +121,41 @@ public class LevelCreator : MonoBehaviour {
                         child.gameObject.transform.position = collectedTiles.transform.FindChild("tmpRoad").transform.position;
                         child.gameObject.transform.parent = collectedTiles.transform.FindChild("tmpRoad").transform;
                         break;
-                    case "killBottom(Clone)":
+                    case "KillBottom(Clone)":
                         child.gameObject.transform.position = collectedTiles.transform.FindChild("tmpKillBottom").transform.position;
                         child.gameObject.transform.parent = collectedTiles.transform.FindChild("tmpKillBottom").transform;
                         break;
-                    case "killTop(Clone)":
+                    case "KillTop(Clone)":
                         child.gameObject.transform.position = collectedTiles.transform.FindChild("tmpKillTop").transform.position;
                         child.gameObject.transform.parent = collectedTiles.transform.FindChild("tmpKillTop").transform;
+                        break;
+                    case "Tree1(Clone)":
+                        child.gameObject.transform.position = collectedTiles.transform.FindChild("tmpTree1").transform.position;
+                        child.gameObject.transform.parent = collectedTiles.transform.FindChild("tmpTree1").transform;
+                        break;
+                    case "Tree2(Clone)":
+                        child.gameObject.transform.position = collectedTiles.transform.FindChild("tmpTree2").transform.position;
+                        child.gameObject.transform.parent = collectedTiles.transform.FindChild("tmpTree2").transform;
+                        break;
+                    case "Cloud1(Clone)":
+                        child.gameObject.transform.position = collectedTiles.transform.FindChild("tmpCloud1").transform.position;
+                        child.gameObject.transform.parent = collectedTiles.transform.FindChild("tmpCloud1").transform;
+                        break;
+                    case "Cloud2(Clone)":
+                        child.gameObject.transform.position = collectedTiles.transform.FindChild("tmpCloud2").transform.position;
+                        child.gameObject.transform.parent = collectedTiles.transform.FindChild("tmpCloud2").transform;
+                        break;
+                    case "Grass1(Clone)":
+                        child.gameObject.transform.position = collectedTiles.transform.FindChild("tmpGrass1").transform.position;
+                        child.gameObject.transform.parent = collectedTiles.transform.FindChild("tmpGrass1").transform;
+                        break;
+                    case "Grass2(Clone)":
+                        child.gameObject.transform.position = collectedTiles.transform.FindChild("tmpGrass2").transform.position;
+                        child.gameObject.transform.parent = collectedTiles.transform.FindChild("tmpGrass2").transform;
+                        break;
+                    case "TreeShadow(Clone)":
+                        child.gameObject.transform.position = collectedTiles.transform.FindChild("tmpTreeShadow").transform.position;
+                        child.gameObject.transform.parent = collectedTiles.transform.FindChild("tmpTreeShadow").transform;
                         break;
                     default:
                         Destroy(child.gameObject);
@@ -95,7 +163,7 @@ public class LevelCreator : MonoBehaviour {
                 }
             }
         }
-        if (gameLayer.transform.childCount < 10)
+        if (gameLayer.transform.childCount < 20)
             fillScene();
         if (roadCounter > 0)
         {
@@ -109,6 +177,13 @@ public class LevelCreator : MonoBehaviour {
     private void fillScene()
     {
         setTile("Road");
+        setTile("Tree1");
+        setTile("Tree2");
+        setTile("Cloud1");
+        setTile("Cloud2");
+        setTile("Grass1");
+        setTile("Grass2");
+        setTile("TreeShadow");
     }
 
     private void setTile(string type)
@@ -117,13 +192,38 @@ public class LevelCreator : MonoBehaviour {
         {
             case "Road":
                 roadCounter++;
-                tmpTile = collectedTiles.transform.FindChild("tmpRoad").transform.GetChild(0).gameObject;
-                tileWidth = 10.5f;
-                tmpTile.transform.parent = gameLayer.transform;
-                tmpTile.transform.position = new Vector3(tileRoadPos.transform.position.x + (tileWidth), startUpRoadPosY, 0);
-                tileRoadPos = tmpTile;
+                tileRoadPos = set("tmpRoad",tileRoadPos, startUpRoadPosY);
+                break;
+            case "Tree1":
+                tileTree1Pos = set("tmpTree1", tileTree1Pos, startUpTree1PosY);
+                break;
+            case "Tree2":
+                tileTree2Pos = set("tmpTree2", tileTree2Pos, startUpTree2PosY);
+                break;
+            case "Cloud1":
+                tileCloud1Pos = set("tmpCloud1", tileCloud1Pos, startUpCloud1PosY);
+                break;
+            case "Cloud2":
+                tileCloud2Pos = set("tmpCloud2", tileCloud2Pos, startUpCloud2PosY);
+                break;
+            case "Grass1":
+                tileGrass1Pos = set("tmpGrass1", tileGrass1Pos, startUpGrass1PosY);
+                break;
+            case "Grass2":
+                tileGrass2Pos = set("tmpGrass2", tileGrass2Pos, startUpGrass2PosY);
+                break;
+            case "TreeShadow":
+                tileTreeShadowPos = set("tmpTreeShadow", tileTreeShadowPos, startUpTreeShadowPosY);
                 break;
         }
+    }
+
+    private GameObject set(string type,GameObject gameObjectType, float PosYType)
+    {
+        tmpTile = collectedTiles.transform.FindChild(type).transform.GetChild(0).gameObject;
+        tmpTile.transform.parent = gameLayer.transform;
+        tmpTile.transform.position = new Vector3(gameObjectType.transform.position.x + (tileWidth), PosYType, 0);
+        return tmpTile;
     }
 
     private void randomizeEnemy()
