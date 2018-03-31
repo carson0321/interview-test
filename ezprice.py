3d9827024ea4d1bfe41de1e10ecdd7c80e661158
carson.wang <carson.wang@droi.com>
2018-03-31 22:39:22 +0800

    clean code, finish POST method

--- ezprice.py
+++ ezprice.py
@@ -15,15 +15,18 @@ app = Flask(__name__)
 
 class User:
     def __init__(self, name, email):
-        if not (name or email):
+        if not name or not email:
             raise ValueError('Empty name, email not allowed.')
         self.id = hashlib.md5((name + email).encode('utf-8')).hexdigest()
         self.name, self.email = name, email
 
+def serializableClass(user):
+    return json.loads(json.dumps(user.__dict__))
+
 users = [
-    json.loads(json.dumps(User('Hello World','0xdeadbeef@gmail.com').__dict__)),
-    json.loads(json.dumps(User('Carson Wang','r03944040@g.ntu.edu.tw').__dict__)),
-    json.loads(json.dumps(User('KaiSheng','r03944040@ntu.edu.tw').__dict__)),
+    serializableClass(User('Hello World','0xdeadbeef@gmail.com')),
+    serializableClass(User('Carson Wang','r03944040@g.ntu.edu.tw')),
+    serializableClass(User('KaiSheng','r03944040@ntu.edu.tw')),
 ]
 
 @app.route('/user/<user_id>', methods=['GET'])
@@ -36,6 +39,19 @@ def getUser(user_id):
 @app.route('/user', methods=['GET'])
 def getAllUser():
     return jsonify({'users': users})
+
+@app.route('/user', methods=['POST'])
+def createUser():
+    if not request.json or not 'name' in request.json or not 'email' in request.json:
+        return make_response(jsonify({'error': 'Bad Request. Must provide parameters {name, email} using JSON schema.'}), 400)
+    name = request.json['name']
+    email = request.json['email']
+    user = User(name, email)
+    exist_user = [i for i in users if i['id'] == user.id]
+    if len(exist_user) > 0:
+        return make_response(jsonify({'error': 'Bad Request. User exists.'}), 400)
+    users.append(serializableClass(user))
+    return jsonify({'message': 'Create user successfully'}), 201
 
 @app.errorhandler(404)
 def notFound(error):
